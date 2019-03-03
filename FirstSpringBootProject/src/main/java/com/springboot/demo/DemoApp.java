45943a39beb4ce8dc45db6d1d8b9201738d12855
MurselPC <MurselPC@DESKTOP-4K9CMLR>
2019-03-03 21:59:08 +0300

    First Commit

---
+++ FirstSpringBootProject/src/main/java/com/springboot/demo/DemoApp.java
@@ -0,0 +1,10 @@
+package com.springboot.demo;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 System.out.println("Hello Spring Boot");
+	}
+
+}
